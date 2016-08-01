abe0aad6702a59d51e2c8dda722075c146dc51ae
SergKolo <1047481448@qq.com>
2016-08-01 15:38:37 -0600

    python script to disable dash when a window is in fullscreen

---
+++ disable_super_key.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+#
+###########################################################
+# Author: Serg Kolo , contact: 1047481448@qq.com 
+# Date: August 1st , 2016
+# Purpose: Disable Super key that calls Unity Dash, when any 
+#          X11 window is in fullscreen state
+# 
+# Written for: http://askubuntu.com/q/805807/295286
+# Tested on: Ubuntu 16.04 LTS 
+###########################################################
+# Copyright: Serg Kolo , 2016
+#    
+#     Permission to use, copy, modify, and distribute this software is hereby granted
+#     without fee, provided that  the copyright notice above and this permission statement
+#     appear in all copies.
+#
+#     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+#     THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#     DEALINGS IN THE SOFTWARE.
+
+import gi
+gi.require_version('Gdk', '3.0')
+from gi.repository import  Gdk,Gio
+import subprocess
+import time
+
+debug = False
+
+def gsettings_get(schema,path,key):
+    if path is None:
+        gsettings = Gio.Settings.new(schema)
+    else:
+        gsettings = Gio.Settings.new_with_path(schema,path)
+    return gsettings.get_value(key)
+
+def gsettings_set(schema,path,key,value):
+    if path is None:
+        gsettings = Gio.Settings.new(schema)
+    else:
+        gsettings = Gio.Settings.new_with_path(schema,path)
+    return gsettings.set_string(key,value)
+
+
+def gsettings_reset(schema,path,key):
+    if path is None:
+        gsettings = Gio.Settings.new(schema)
+    else:
+        gsettings = Gio.Settings.new_with_path(schema,path)
+    return gsettings.reset(key)
+
+def run_cmd(cmdlist):
+    try:
+        stdout = subprocess.check_output(cmdlist)
+    except subprocess.CalledProcessError:
+        pass
+    else:
+        if stdout:
+            return stdout
+
+
+def main():
+    screen = Gdk.Screen.get_default()
+    while True:
+        
+        key_state = str(gsettings_get('org.compiz.unityshell', 
+                                  '/org/compiz/profiles/unity/plugins/unityshell/', 
+                                  'show-launcher'))
+        active_xid = str(screen.get_active_window().get_xid())
+        wm_state =  run_cmd( ['xprop', '-root', '-notype','-id',active_xid, '_NET_WM_STATE'])  
+        
+        if debug : print key_state,wm_state
+
+        if 'FULLSCREEN' in wm_state:
+            if "Super" in  key_state:    
+                gsettings_set('org.compiz.unityshell', 
+                              '/org/compiz/profiles/unity/plugins/unityshell/',
+                              'show-launcher', 
+                              'Disabled')
+        
+        else:
+            if "Disabled" in key_state :
+               gsettings_reset( 'org.compiz.unityshell', 
+                                '/org/compiz/profiles/unity/plugins/unityshell/',
+                                'show-launcher')
+    
+    
+        time.sleep(0.25)
+
+
+if __name__ == "__main__":
+    main()
