7894f845d01b211ea5247ddc827ffac25ded1c04
SergKolo <1047481448@qq.com>
2016-08-22 18:14:59 -0600

    simplified script,removed redundant lines

--- chgreeterbg.sh
+++ chgreeterbg.sh
@@ -20,77 +20,65 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 # DEALINGS IN THE SOFTWARE.
 #set -x
-function printUsage
+print_usage()
 {
   printf "\n!!! %s\n%s\n" "Usage:" "sudo script.sh /path/to/image"
   exit 1
 }
 
-function createOR
+create_override_file()
 {
-  touch "$1"
+  # ensure image is readable
+  chmod +r "$image_file"
+
+  # Ensure file exists with required information
   { printf "%s\n" "[com.canonical.unity-greeter]" ;
     printf "%s\n" "draw-user-backgrounds=false" ;
-    printf "%s\n" "background=" ;
+    printf "%s\n" "background='$2'" ;
   } > "$1"
+
+  glib-compile-schemas "$3"
 }
-
-function changeBG
-{
-   new_image="'$1'"
-   printf "Changing background to %s\n"  "$new_image"
-   sed -i 's;background=.*;background='"$new_image"';g' "$2"
-   printf "Recompiling schemas\n"
-   glib-compile-schemas /usr/share/glib-2.0/schemas/  
-   printf "Done. Preview changes with dm-tool switch-to-greeter command"
-}
-
-
-###################
-# MAIN
-###################
 
 main()
 {
-   ARGC=$#
-   ARGV="$*"
-   local IMAGE="$(readlink -e "$ARGV" 2>/dev/null )" 
-         # full path to existing image
-   local ORFILE="/usr/share/glib-2.0/schemas/99_unity_greeter_background.gschema.override" 
-         # override file for unity greeter glib schema
 
-   if [ -z $ARGV ];then 
-       IMAGE="$(zenity --file-selection --filename='/home' )"
-   fi
+    ARGC=$#
+    ARGV="$@"
 
-   if [ -z "$IMAGE" ];then
-      exit 1
-   fi
-
-  # make sure image is readable by non-owner
-  chmod +r "$IMAGE"
-
-  # check if the override file exists, else create it
-  #[ -f "$ORFILE" ] || 
-  createOR "$ORFILE"
-
-  # actually change greeter background
-  changeBG "$IMAGE" "$ORFILE"
-  if [ $? -eq 0  ] && [ -z "$ARGV"   ];then
-     zenity --info --text="Done. Preview changes with dm-tool switch-to-greeter command"
-  fi
+    local image_file="$(readlink -e "$ARGV" 2>/dev/null )" 
+    local schemas_dir="/usr/share/glib-2.0/schemas/"
+    local override_file="$schemas_dir/99_unity_greeter_background.gschema.override" 
+    
+    if [ -z $ARGV ];then 
+        image_file="$(zenity --file-selection --filename='/home' )"
+    fi
+    
+    if [ -z "$image_file" ] || ! [ -f "$image_file"  ] ;then
+        exit 1
+    fi
+    
+    create_override_file "$override_file" "$image_file" "$schemas_dir"
+    
+    text="Done. Preview changes with 'dm-tool switch-to-greeter' command"
+    if [ $? -eq 0  ] && [ -z "$ARGV"   ];then
+        zenity --info --text="$text"
+    else
+        printf "\n%s\n" "$text"
+    fi
 }
 
 # check if we're root, else quit
 if [ $( id -u ) -eq 0 ];then
-     main "$@" 
+    main "$@" 
 else
-  if [ $# -eq 0   ];then
-     zenity --password | sudo -S "$(readlink -e  $0)"  || exit 1
-  else
-       sudo "$(readlink -e $0)" "$(readlink -q -e "$@" )"
-  fi
+    # calling script without arguments spawns GUI dialog
+    if [ $ARGC -eq 0   ];then
+        zenity --password | \
+               sudo -S "$(readlink -e  $0)" -p "" \
+               || exit 1
+    else
+        sudo "$(readlink -e $0)" "$(readlink -q -e "$@" )"
+    fi
 fi
-
-
 
